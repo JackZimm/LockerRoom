298e222bb354b409a67b2060d87b78929828d649
JackZimm <107709662+JackZimm@users.noreply.github.com>
2024-05-29 11:45:39 -0400

    Create banner.php

---
+++ includes/banner.php
@@ -0,0 +1,15 @@
+
+<div class="banner">
+	<div class="welcome_msg">
+		<h1>Welcome to the Locker Room!</h1>
+	</div>
+	<!--<div class="login_div">
+		<form action="index.php" method="post">
+			<h2>Login</h2>
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="password"  placeholder="Password"> 
+			<button class="btn" type="submit" name="login_btn">Sign in</button>
+		</form>
+		<p>Not a member yet?<a href ="register.php"> Sign Up </a>here!</p>
+	</div>-->
+</div>
