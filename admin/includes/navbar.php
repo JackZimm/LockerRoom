e8de3f57b869d7477a9880b9e8351de78ca63b52
JackZimm <107709662+JackZimm@users.noreply.github.com>
2024-05-29 11:52:15 -0400

    Create navbar.php

---
+++ admin/includes/navbar.php
@@ -0,0 +1,13 @@
+<div class="header">
+	<div class="logo">
+		<a href="<?php echo BASE_URL .'/admin/dashboard.php' ?>">
+			<h1>Locker Room Sports - Admin</h1>
+		</a>
+	</div>
+	<div class="user-info">
+		
+		<a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout-btn">Log Out</a>
+		<a href = "<?php echo BASE_URL?>" class = "logout-btn2">Home</a>
+		
+	</div>
+</div>
